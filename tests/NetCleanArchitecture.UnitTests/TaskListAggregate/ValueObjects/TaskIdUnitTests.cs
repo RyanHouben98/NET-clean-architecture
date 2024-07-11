69d76e3ad64424f55de296188dc8cc95dc01ce29
Ryan Houben <connect@ryanhouben.com>
2024-07-11 08:28:53 +0200

    Add TaskIdUnitTests

---
+++ tests/NetCleanArchitecture.UnitTests/TaskListAggregate/ValueObjects/TaskIdUnitTests.cs
@@ -0,0 +1,34 @@
+﻿namespace NetCleanArchitecture.Domain.UnitTests;
+
+public class TaskIdUnitTests
+{
+    [Fact]
+    public void CreateUnique_Valid_ShouldReturnNewTaskId()
+    {
+        // Act
+        var result = TaskId.CreateUnique();
+
+        // Assert
+        Assert.Multiple(() => {
+            Assert.IsType<TaskListId>(result);
+            Assert.NotEqual(Guid.NewGuid(), result.Value);
+        });
+
+    }
+
+    [Fact]
+    public void Create_Valid_ShouldReturnInitializedTaskId()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        // Act
+        var result = TaskId.Create(id);
+
+        // Assert
+        Assert.Multiple(() => {
+            Assert.IsType<TaskListId>(result);
+            Assert.Equal(id, result.Value);
+        });
+    }
+}
